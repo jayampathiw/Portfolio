c96108da764959f6b809fed0bc3e3d4b5d1abe25
Jayampathy Wijesena <udayanga5@gmail.com>
2024-09-10 10:51:11 +0530

    time line update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -2,77 +2,157 @@
 
 import Link from "next/link";
 import { BsArrowDownRight } from "react-icons/bs";
-
+import React from "react";
+import { BsBriefcase } from "react-icons/bs";
 import { motion } from "framer-motion";
 
-const servicesList = [
+const timelineData = [
   {
-    num: "01",
-    title: "Web Development",
+    id: 1,
+    date: "2022 - Present",
+    company: "Tech Innovations Inc.",
+    position: "Senior Software Engineer",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quasi. Hic ab dolores minus dignissimos qui pariatur! .",
-    href: "",
+      "Leading development of cloud-based solutions and mentoring junior developers.",
   },
   {
-    num: "02",
-    title: "UI/UX Design",
+    id: 2,
+    date: "2019 - 2022",
+    company: "DataDrive Solutions",
+    position: "Full Stack Developer",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quasi. Hic ab dolores minus dignissimos qui pariatur! .",
-    href: "",
+      "Developed and maintained multiple web applications using React and Node.js.",
   },
   {
-    num: "03",
-    title: "Log Design",
+    id: 3,
+    date: "2017 - 2019",
+    company: "StartUp Nexus",
+    position: "Junior Developer",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quasi. Hic ab dolores minus dignissimos qui pariatur! .",
-    href: "",
+      "Assisted in the development of mobile apps and gained experience in Agile methodologies.",
   },
   {
-    num: "04",
-    title: "Connect",
+    id: 2,
+    date: "2019 - 2022",
+    company: "DataDrive Solutions",
+    position: "Full Stack Developer",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quasi. Hic ab dolores minus dignissimos qui pariatur! .",
-    href: "",
+      "Developed and maintained multiple web applications using React and Node.js.",
+  },
+  {
+    id: 3,
+    date: "2017 - 2019",
+    company: "StartUp Nexus",
+    position: "Junior Developer",
+    description:
+      "Assisted in the development of mobile apps and gained experience in Agile methodologies.",
   },
 ];
+
+const TimelineItem = ({ data, index }) => {
+  const isEven = index % 2 === 0;
+  return (
+    <div
+      className={`mb-8 flex flex-col sm:flex-row justify-between items-start sm:items-center w-full ${
+        isEven ? "sm:flex-row-reverse" : ""
+      }`}
+    >
+      <div className="order-1 w-full sm:w-5/12 mb-4 sm:mb-0">
+        <div
+          className={`px-4 py-4 ${isEven ? "sm:text-right" : "sm:text-left"}`}
+        ></div>
+      </div>
+      <div className="z-20 flex items-center justify-center order-first sm:order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full mb-4 sm:mb-0 ml-1">
+        <BsBriefcase />
+      </div>
+      <div className="order-last sm:order-1 rounded-lg shadow-xl w-full sm:w-5/12 px-6 py-4 bg-[#232329]">
+        <div className="flex justify-between">
+          <div className="w-full">
+            <p className="text-md font-bold text-accent">{data.date}</p>
+          </div>
+          <div>
+            <Link
+              href=""
+              className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+            >
+              <BsArrowDownRight className="text-primary text-3xl" />
+            </Link>
+          </div>
+        </div>
+
+        <h3 className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+          {data.company}
+        </h3>
+        <h4 className="text-xl sm:text-[25px] font-bold text-white">
+          {data.position}
+        </h4>
+        <p className="text-white/60">{data.description}</p>
+      </div>
+    </div>
+  );
+};
 
 const Services = () => {
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-      <div className="container mx-auto">
+      <div className="container mx-auto w-full">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
           }}
-          className="grid grid-cols-1 xl:grid-cols-2 gap-[60px]"
+          className="grid grid-cols-1 gap-[60px]"
         >
-          {servicesList.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="flex-1 flex flex-col justify-center gap-6 group"
-              >
-                <div className="w-full flex justify-between items-center ">
-                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
-                    {item.num}
+          <div className="relative wrap overflow-hidden p-4 sm:p-10">
+            {timelineData.map((data, index) => {
+              const isEven = index % 2 === 0;
+              return (
+                <div
+                  className={`mb-8 flex flex-col sm:flex-row justify-between items-start sm:items-center w-full group ${
+                    isEven ? "sm:flex-row-reverse" : ""
+                  }`}
+                >
+                  <div class="flex justify-center absolute  h-full left-4 sm:left-1/2">
+                    <div class="absolute inset-y-0 w-px bg-gray-700 opacity-20"></div>
+                    <div class="absolute top-1/2 transform -translate-y-1/2">
+                      <div class="w-10 h-10 rounded-full bg-gray-800 flex items-center justify-center z-10">
+                        <span class="text-white text-xl">
+                          <BsBriefcase />
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                  <Link
-                    href={item.href}
-                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
-                  >
-                    <BsArrowDownRight className="text-primary text-3xl" />
-                  </Link>
+
+                  <div className="order-last sm:order-1 rounded-lg shadow-xl w-full sm:w-5/12 px-6 py-4 bg-[#232329]">
+                    <div className="flex justify-between">
+                      <div className="w-full">
+                        <p className="text-md font-bold text-accent">
+                          {data.date}
+                        </p>
+                      </div>
+                      <div>
+                        <Link
+                          href=""
+                          className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                        >
+                          <BsArrowDownRight className="text-primary text-3xl" />
+                        </Link>
+                      </div>
+                    </div>
+
+                    <h3 className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                      {data.company}
+                    </h3>
+                    <h4 className="text-xl sm:text-[25px] font-bold text-white">
+                      {data.position}
+                    </h4>
+                    <p className="text-white/60">{data.description}</p>
+                  </div>
                 </div>
-                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
-                  {item.title}
-                </h2>
-                <p className="text-white/60">{item.description}</p>
-                <div className="border-b border-white/20 w-full"></div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </motion.div>
       </div>
     </section>
