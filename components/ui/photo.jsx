a3230c497ee6ab9b044b2b87d6ad5780d3e5d7d8
Jayampathy Wijesena <udayanga5@gmail.com>
2024-11-03 21:17:08 +0530

    Update photo.jsx

--- components/ui/photo.jsx
+++ components/ui/photo.jsx
@@ -23,7 +23,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
         >
           <Image
-            src={`${prefix}/assets/profile-photo.png`}
+            src={`./assets/profile-photo.png`}
             priority
             quality={100}
             fill
